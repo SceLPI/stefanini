7ad613efb47bf0f0df11fc6e9e0018cfe42659cd
SceLPI <l.eduardosoares@gmail.com>
2025-06-04 02:27:22 -0300

    Updates Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,20 @@ else
   echo "vendor/ already exists, jumping"
 fi
 
+touch /var/log/php_errors.log
+chmod 777 /var/log/php_errors.log
+echo "log_errors=On" > /usr/local/etc/php/conf.d/docker-fpm.ini
+echo "error_log=/var/log/php_errors.log" >> /usr/local/etc/php/conf.d/docker-fpm.ini
+echo "display_errors=On" >> /usr/local/etc/php/conf.d/docker-fpm.ini
+echo "display_startup_errors=On" >> /usr/local/etc/php/conf.d/docker-fpm.ini
+echo "error_reporting=E_ALL" >> /usr/local/etc/php/conf.d/docker-fpm.ini
+
+cp .env.example .env
+php artisan key:generate
+rm -rf /var/www/html/storage/logs/laravel.log
+touch /var/www/html/storage/logs/laravel.log
+chown -R www-data:www-data /var/www/html
+
 echo "xdebug.mode=coverage" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 php artisan migrate --force
 php-fpm &
